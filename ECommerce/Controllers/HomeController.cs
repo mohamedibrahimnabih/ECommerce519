ee6d00f47ba71ba5581b40fab960de8ae59368d7
mohamedibrahimnabih <eng.mohamedibrahimnabih@gmail.com>
2025-09-18 22:45:02 +0300

    First Action

--- ECommerce/Controllers/HomeController.cs
+++ ECommerce/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using ECommerce.Models;
+using ECommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.Controllers
@@ -22,6 +23,41 @@ namespace ECommerce.Controllers
         {
             return View();
         }
+
+        public ViewResult Welcome()
+        {
+            return View();
+        }
+
+        public ViewResult PersonalInfo()
+        {
+            List<Person> persons = new List<Person>();
+            persons.AddRange(new()
+            {
+                Id = 1,
+                Name = "Mohamed",
+                Salary = 1000,
+                Address = "Caio"
+            }, new()
+            {
+                Id = 2,
+                Name = "Ali",
+                Salary = 2000,
+                Address = "Alex"
+            });
+
+            // Logic
+
+            var personsInDB = persons.AsQueryable();
+            var totalPersons = persons.Count();
+
+            return View(new PersonVM
+            {
+                Persons = personsInDB.ToList(),
+                TotalPerson = totalPersons
+            });
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ECommerce/ViewModels/PersonVM.cs
@@ -0,0 +1,10 @@
+﻿using ECommerce.Models;
+
+namespace ECommerce.ViewModels
+{
+    public class PersonVM
+    {
+        public List<Person> Persons { get; set; }
+        public int TotalPerson { get; set; }
+    }
+}
